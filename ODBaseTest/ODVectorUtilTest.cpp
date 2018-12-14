b2476cc204da2e1ae10b521ca0b7fa3e477b6a3f
odddd0 <odddd0@github.com>
2018-12-14 15:14:59 +0800

    [Test] off all none use test

--- ODBaseTest/ODVectorUtilTest.cpp
+++ ODBaseTest/ODVectorUtilTest.cpp
@@ -10,12 +10,12 @@
 
 #include <ODUtil/ODVectorUtil.h>
 
-TEST(ODVectorUtil, RefreshInsert)
-{
-    StringList tmpList;
-    ODVectorUtil::RefreshInsert<std::string>(tmpList, "1");
-    ODVectorUtil::RefreshInsert<std::string>(tmpList, "2");
-    ODVectorUtil::RefreshInsert<std::string>(tmpList, "3");
-    ODVectorUtil::RefreshInsert<std::string>(tmpList, "2");
-    ODVectorUtil::RefreshInsert<std::string>(tmpList, "1");
-}
+//TEST(ODVectorUtil, RefreshInsert)
+//{
+//    StringList tmpList;
+//    ODVectorUtil::RefreshInsert<std::string>(tmpList, "1");
+//    ODVectorUtil::RefreshInsert<std::string>(tmpList, "2");
+//    ODVectorUtil::RefreshInsert<std::string>(tmpList, "3");
+//    ODVectorUtil::RefreshInsert<std::string>(tmpList, "2");
+//    ODVectorUtil::RefreshInsert<std::string>(tmpList, "1");
+//}
